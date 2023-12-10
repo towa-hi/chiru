6584e248c9313589bb05590b907803e96f52b361
why <whyperyawn@gmail.com>
2023-12-09 18:27:44 -0600

    i need more boulettes

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Gun : MonoBehaviour
+{
+    public GameObject projectilePrefab;
+    public Transform fireOrigin;
+    public Transform fireDestination;
+    public int numberOfProjectiles = 1;
+    public float spreadAngle = 30f;
+    public float fireRate = 0.5f;
+
+    private float nextFireTime = 0f;
+
+    void Update()
+    {
+        
+        if (Mouse.current.leftButton.isPressed && Time.time >= nextFireTime)
+        {
+            Shoot();
+            nextFireTime = Time.time + 1f / fireRate;
+        }
+    }
+
+    void Shoot()
+    {
+        Vector3 direction = (fireDestination.position - fireOrigin.position).normalized;
+        float angle = Mathf.Atan2(direction.z, direction.x) * Mathf.Rad2Deg;
+        
+        for (int i = 0; i < numberOfProjectiles; i++)
+        {
+            float currentAngle = angle - (spreadAngle / 2) + (i * spreadAngle / (numberOfProjectiles - 1));
+            Quaternion rotation = Quaternion.Euler(0f, currentAngle, 0f);
+
+            Vector3 bulletDirection = new Vector3(Mathf.Cos(currentAngle * Mathf.Deg2Rad), 0f, Mathf.Sin(currentAngle * Mathf.Deg2Rad));
+            Quaternion bulletRotation = Quaternion.LookRotation(bulletDirection, Vector3.up);
+
+            Vector3 spawnPosition = fireOrigin.position;
+
+            Instantiate(projectilePrefab, spawnPosition, bulletRotation);
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float speed = 10f;
+    public float lifetime = 2f;
+    public Vector3 angle = Vector3.zero;
+
+    private void Start()
+    {
+        Destroy(gameObject, lifetime);
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+}
