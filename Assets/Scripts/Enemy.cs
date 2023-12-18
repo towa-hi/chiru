7dff3628228c04f2693e13a6ada335fefe1c2564
towa-hi <yamyamasunday@gmail.com>
2023-12-18 17:52:45 +0900

    theo

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float hearingRadius;
+    public float visionRadius;
+    public float visionFOV;
+
+    public float hp;
+    public float movespeed;
+    
+}
